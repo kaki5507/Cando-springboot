a35d184d5710f9820ec679ec5c02519c5c4bbd76
kaki5507 <ckdbioksh@naver.com>
2021-10-06 01:10:40 +0900

    Feat: Google OAuth 구현
    - 구글 인증 부분을 구현
    - 구글 서비스 등록
    - User 클래스 만들었다

--- src/main/java/com/goodee/cando/web/IndexController.java
+++ src/main/java/com/goodee/cando/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.goodee.cando.web;
 
+import com.goodee.cando.config.auth.dto.SessionUser;
 import com.goodee.cando.service.posts.PostsService;
 import com.goodee.cando.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
 
         return "index";
     }
